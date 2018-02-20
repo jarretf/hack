ef6c798e2bad766c55eb99c7bb63eee33fbfc583
SG0226079 <ramanjaneyulu.mukkoti@sabre.com>
2018-02-20 17:07:53 +0530

    initial files

---
+++ client/app/_models/Reservation.ts
@@ -0,0 +1,15 @@
+export class Reservation {
+    confirmNo: string;
+    guestName: string;
+    status: string;
+    startDate : date;
+    endDate : date;
+    roomNo : number;
+    contactNo: number;
+    imageUrl : string;
+    balance : number;
+    paymentType : string;
+    guests : number;
+    reservationType : string;
+    profileId : string;
+}
---
+++ client/app/home/home.component.ts
@@ -0,0 +1,51 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+import { Reservation } from '../_models/index';
+import { UserService } from '../_services/index';
+
+
+@Component({
+    moduleId: module.id,
+    templateUrl: 'home.component.html'
+})
+
+export class HomeComponent implements OnInit {
+    currentUser: User;
+    reservation : Reservation;
+    reservations : Reservation[] = [];
+
+    constructor(private userService: UserService) {
+        //this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    }
+
+    ngOnInit() {
+        this.reservations = this.mockData();
+    }
+
+    deleteUser(_id: string) {
+        this.userService.delete(_id).subscribe(() => { this.loadAllUsers() });
+    }
+
+
+    private mockData(){
+        var reservations = [{
+            confirmNo : 'CRSXX12345',
+            guestName : 'Mukkoti',
+            status : 'In House',
+            startDate : 'Jan 15, 2015',
+            endDate : 'Feb 23, 2015',
+            roomNo : 325,
+            contactNo : '9986188000',
+            imageUrl : 'https://www.sabre.com/wp/wp-content/themes/sabre-corp/images/sabre-logo-slab.svg'
+            balance : 345,
+            paymentType : 'Credit Card',
+            guests : 2,
+            reservationType : 'GROUP',
+            profileId : '436744775335'
+        }];
+
+        return reservations;
+    }
+
+
+}
